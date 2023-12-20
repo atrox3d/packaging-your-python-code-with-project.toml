4da81ed12520f1a5311ec1c8d03fe92001f83c76
atrox3d <atrox3d@gmail.com>
2023-12-20 15:26:56 +0100

    alternate problem using -m or using relative imports

--- snakesay/__main__.py
+++ snakesay/__main__.py
@@ -1,5 +1,11 @@
 import sys
 
-import snake
+print(f'{sys.path[0] = }')
 
-snake.say(" ".join(sys.argv[1:]))
+try:
+    # import snake
+    from snakesay import snake
+
+    snake.say(" ".join(sys.argv[1:]))
+except ModuleNotFoundError as mnfe:
+    print(f'{mnfe!r}')    
