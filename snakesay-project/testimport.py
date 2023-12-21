2f47447c25956e40228c55230d56f815e3305af9
atrox3d <atrox3d@gmail.com>
2023-12-21 20:56:36 +0100

    test imports to check errors

--- snakesay-project/testimport.py
+++ snakesay-project/testimport.py
@@ -1,37 +1,57 @@
 import os, sys
+import traceback
 
 filename = os.path.basename(__file__)
 
+def decorator(func):
+    def wrapper():
+        try:
+            funcname = func.__name__
+            module, say = func()
+            modulename = module.__name__
+            moduleattribs = [item for item in dir(module) if not item.startswith("_")]
+            print(f'{funcname} | dir({modulename}) | {moduleattribs}')
+            say(f'{filename}:{funcname}')
+            # del sys.modules["moduleName"] 
+            # del modulename
+            for key in [k for k in sys.modules.keys()]:
+                if key.startswith('snake'):
+                    del sys.modules[key]
+            del module
+            # print(sys.modules.keys())
+        except Exception as e:
+            print(f'{funcname}: {e!r}')
+            # print(traceback.format_exc())
+            # raise
+
+    return wrapper
+
+# @decorator
+# def test():
+#     import snakesay
+#     return snakesay.snake, snakesay.snake.say
+
+@decorator
 def from_snakesay():
-    name = from_snakesay.__name__
     from snakesay import snake
-    print(f'{name} | dir(snake) | {[item for item in dir(snake) if not item.startswith("_")]}')
-    snake.say(f'{filename}:{name}')
+    return snake, snake.say
 
+@decorator
 def import_snakesay_snake():
-    # import snakesay as ss
-    name = import_snakesay_snake.__name__
     import snakesay.snake
-    print(f'{name} | dir(snakesay.snake) | {[item for item in dir(snakesay.snake) if not item.startswith("_")]}')
-    snakesay.snake.say(f'{filename}:{name}')
+    return snakesay.snake, snakesay.snake.say
 
+@decorator
 def import_snakesay():
-    name = import_snakesay.__name__
-    print(f'{name} | import snakesay')
     import snakesay
-    print(f'{name} | dir(snakesay) | {[item for item in dir(snakesay) if not item.startswith("_")]}')
-    # print(f'{snakesay.__file__ = }')
-    snakesay.snake.say(f'{filename}:{name}')
+    return snakesay.snake, snakesay.snake.say
 
 def main(fn):
-    try:
-        fn()
-    except Exception as e:
-        print(f'{fn.__name__}: {e!r}')
-        # quit()
+    fn()
 
 if __name__ == '__main__':
     print(f'{sys.path[0:2] = }')
-    main(import_snakesay)
-    main(import_snakesay_snake)
     main(from_snakesay)
+    main(import_snakesay_snake)
+    main(import_snakesay)
+    # main(test)
